099d71416a95f75378666de1a38275912a7d8b95
Veeteq <witold.wojnarowicz@gmail.com>
2026-08-15 18:52:26 +0200

    BUG-002: RSA key pair generation issue

--- authorization-app/src/main/java/com/veeteq/auth/authservice/config/JwtConfig.java
+++ authorization-app/src/main/java/com/veeteq/auth/authservice/config/JwtConfig.java
@@ -3,8 +3,6 @@ package com.veeteq.auth.authservice.config;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
